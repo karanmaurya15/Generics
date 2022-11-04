46d1550e6295409a171f56847a489e8994272a7c
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-04 14:35:54 +0530

    Wellcome Program

---
+++ Generics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Generics program");
+        }
+    }
+}
