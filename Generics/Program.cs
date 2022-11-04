182201f24ac4d8e9d3fbbe9f9aec9851176649df
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-04 15:00:30 +0530

    Print Array Sample Code is Done

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -2,9 +2,48 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void toPrint(int[] inputArray)
         {
-            Console.WriteLine("Wellcome to Generics program");
+
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------------------------");
         }
+
+        public static void toPrint(double[] inputArray)
+        {
+
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------------------------");
+        }
+
+        public static void toPrint(string[] inputArray)
+        {
+
+            foreach (string element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------------------------------");
+        }
+    
+       static void Main(string[] args)
+       {
+            Console.WriteLine("Wellcome to Generics program\n");
+
+            int[] intArray = { 9, 8, 7, 6, 5, 3};
+            double[] doubleArray = { 9.6, 7.5, 8.4, 5.33 };
+            string[] stringArray = { "Generic", "OOPS", "Addressbook" };
+
+            Program.toPrint(intArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(stringArray);
+
+       }
     }
 }
